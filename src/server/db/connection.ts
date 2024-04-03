55364a4aef1922ddbda044c55efb4d59c26b4bbe
MD <mdeanwill@gmail.com>
2024-04-03 10:48:26 -0700

    added env vars

--- src/server/db/connection.ts
+++ src/server/db/connection.ts
@@ -1,10 +1,6 @@
 import mysql from 'mysql2/promise';
+import config from '../config';
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'chirper_user',
-    password: 'password123',
-    database: 'chirper'
-});
+const pool = mysql.createPool(config.db);
 
 export default pool;
